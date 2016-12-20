c718ac367a1b9a3837665d4c17dc3e93bf2df99c
minhaz <minhazav@gmail.com>
2016-12-20 23:42:57 +0530

    init code

---
+++ app/src/main/java/xyz/minhazav/androidexperiments/RepoElement.java
@@ -0,0 +1,15 @@
+package xyz.minhazav.androidexperiments;
+
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+import android.content.Context;
+
+/**
+ * Created by minhazv on 12/19/2016.
+ */
+
+public class RepoElement extends LinearLayout {
+    public RepoElement(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
